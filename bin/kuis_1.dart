7b68b4b174bca422e84dbcd67d0eb5d50de8a4f2
taufiqyfirdaus <taufiqyfirdaus@gmail.com>
2023-10-02 13:18:27 +0700

    first commit

---
+++ bin/kuis_1.dart
@@ -0,0 +1,5 @@
+import 'package:kuis_1/kuis_1.dart' as kuis_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kuis_1.calculate()}!');
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+// import 'package:flutter/material.dart';
+
+void main() {
+  // Tugas 1
+  print('Tugas 1');
+  print('Hello world');
+
+  print('');
+
+  // Tugas 2
+  print('Tugas 2');
+  String name = 'Taufiqy Firdaus Jatu';
+
+  print(name);
+  print(name);
+  print(name);
+
+  print('');
+
+  // Tugas 3
+  print('Tugas 3');
+  String firsName = 'Taufiqy';
+  final lastName = 'Firdaus Jatu';
+
+  print(firsName);
+  print(lastName);
+
+  print('');
+
+  // Tugas 4
+  print('Tugas 4a');
+  final array1 = [1, 2, 3];
+  const array2 = [4, 5, 6];
+
+  array1[0] = 100;
+  array2[0] = 100;
+
+  print(array1);
+  print(array2);
+
+  print('');
+  // Tugas 4
+
+  print('Tugas 4b');
+  int number1 = 100;
+  double number2 = 100.5;
+
+  print(number1);
+  print(number2);
+
+  print('');
+
+  // Tugas 5
+  print('Tugas 5');
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+  print('');
+
+  // Tugas 6
+  print('Tugas 6');
+  String firstName = 'Taufiqy';
+  String LastName = 'Firdaus Jatu';
+
+  var fullName = '$firstName ${LastName}';
+
+  print(fullName);
+
+  print('');
+
+  // Tugas 7
+  print('Tugas 7');
+  var text = 'ini \'OPTIMAL\' \$sekali';
+  print(text);
+
+  print('');
+
+  // Tugas 8
+  print('Tugas 8');
+  var name1 = firstName + lastName;
+  var name2 = 'Taufiqy' 'Firdaus' 'Jatu';
+  print(name1);
+  print(name2);
+
+  print('');
+
+  // Tugas 9
+  print('Tugas 9');
+  var nama1 = firstName + lastName;
+  var nama2 = 'Taufiqy' 'Firdaus' 'Jatu';
+  print(nama1);
+  print(nama2);
+
+  print('');
+
+  // Tugas 10
+  print('Tugas 10');
+  var kalimat_panjang = '''
+  ini
+  saya punya
+  proyek dart
+  banyak ''';
+  print(kalimat_panjang);
+}
